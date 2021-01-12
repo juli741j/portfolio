8a77dfb351e10a04421c6a06b93a0fba5963ecd9
Julie Eggertsen <Julieeggertsen1@Christinas-MacBook-Pro.local>
2021-01-12 14:35:22 +0100

    Færdig portfolio

--- forside.js
+++ forside.js
@@ -1,8 +1,8 @@
-document.querySelector("#menuknap").addEventListener("click", toggleMenu)
+document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 
 function toggleMenu() {
     console.log("toggleMenu");
-    document.querySelector("#menu1").classList.toggle("hidden")
+    document.querySelector("#menu1").classList.toggle("hidden");
 
     let erSkjult = document.querySelector("#menu1").classList.contains("hidden");
 
@@ -11,4 +11,22 @@ function toggleMenu() {
     } else {
         document.querySelector("#menuknap").textContent = "X";
     }
+}
+
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {
+        slideIndex = 1;
+    }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000);
 }
